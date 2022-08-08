c9063abff8636dd54329a30f51a0054b0c07bd1b
unknown <ali.javanmard14@gmail.com>
2022-08-08 19:07:14 +0430

    editting proxy3-5

--- proxy.py
+++ proxy.py
@@ -68,66 +68,6 @@ def thread_runner(client_socket: socket.socket, server_socket: socket.socket,  a
             proxy_request(server_socket, client_socket, data)
 
 
-def sending_packets(self):
-    try:
-        sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-        sock.settimeout(self.to)
-        sock.connect((self.tgt, int(self.port)))
-        self.pkt_count += 3
-        if sock:
-            sock.sendall('X-a: b\r\n')
-            self.pkt += 1
-    except Exception:
-        sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-        sock.settimeout(self.to)
-        sock.connect((self.tgt, int(self.port)))
-        sock.settimeout(None)
-        if sock:
-            sock.sendall('X-a: b\r\n')
-            self.pkt_count += 1
-    except KeyboardInterrupt:
-        sys.exit(cprint('[-] Canceled by user', 'red'))
-    return sock
-
-
-def doconnection(self):
-    socks = 0
-    fail = 0
-    lsocks = []
-    lhandlers = []
-    print('\t\tBuilding sockets', 'blue')
-    while socks < (int(self.threads)):
-        try:
-            sock = self.building_socket()
-            if sock:
-                lsocks.append(sock)
-                socks += 1
-                if socks > int(self.threads):
-                    break
-        except Exception:
-            fail += 1
-        except KeyboardInterrupt:
-            sys.exit(cprint('[-] Canceled by user', 'red'))
-    print('\t\tSending packets', 'blue')
-    while socks < int(self.threads):
-        try:
-            handler = self.sending_packets()
-            if handler:
-                lhandlers.append(handler)
-                socks += 1
-                if socks > int(self.threads):
-                    break
-            else:
-                pass
-        except Exception:
-            fail += 1
-        except KeyboardInterrupt:
-            break
-            sys.exit(cprint('[-] Canceled by user', 'red'))
-    # print colored('I have sent ','green') + colored(str(self.pkt_count),'cyan') + colored(' packets successfully.Now i\'m going to sleep for ','green') + colored(self.sleep,'red') + colored(' second','green')
-    time.sleep(self.sleep)
-
-
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.connect((HOST, PORT))
